b6871b845ab01c831e5a37b81428869535050a0c
Chris Mucciolo <564769@bah.com>
2019-02-19 17:14:55 -0500

    finished initial getting started tutorial using gqlgen

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gql-sandbox
 messing around with grapql in go
+
+Initially, created example to-do list graphql server in go using this [getting started tutorial](https://gqlgen.com/getting-started/)
--- resolver.go
+++ resolver.go
@@ -2,9 +2,14 @@ package gql_sandbox
 
 import (
 	"context"
+	"fmt"
+	"github.com/chrism00ch/gql-sandbox/gettingstarted"
+	"math/rand"
 )
 
-type Resolver struct{}
+type Resolver struct {
+	todos []gettingstarted.Todo
+}
 
 func (r *Resolver) Mutation() MutationResolver {
 	return &mutationResolver{r}
@@ -12,15 +17,30 @@ func (r *Resolver) Mutation() MutationResolver {
 func (r *Resolver) Query() QueryResolver {
 	return &queryResolver{r}
 }
+func (r *Resolver) Todo() TodoResolver {
+	return &todoResolver{r}
+}
 
 type mutationResolver struct{ *Resolver }
 
-func (r *mutationResolver) CreateTodo(ctx context.Context, input NewTodo) (Todo, error) {
-	panic("not implemented")
+func (r *mutationResolver) CreateTodo(ctx context.Context, input NewTodo) (gettingstarted.Todo, error) {
+	todo := gettingstarted.Todo{
+		Text:   input.Text,
+		ID:     fmt.Sprintf("T%d", rand.Int()),
+		UserID: input.UserID,
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
 }
 
 type queryResolver struct{ *Resolver }
 
-func (r *queryResolver) Todos(ctx context.Context) ([]Todo, error) {
-	panic("not implemented")
+func (r *queryResolver) Todos(ctx context.Context) ([]gettingstarted.Todo, error) {
+	return r.todos, nil
+}
+
+type todoResolver struct{ *Resolver }
+
+func (r *todoResolver) User(ctx context.Context, obj *gettingstarted.Todo) (User, error) {
+	return User{ID: obj.UserID, Name: "user " + obj.UserID}, nil
 }
